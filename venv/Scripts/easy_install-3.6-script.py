0f1153f4f4c26b811d26a0560d871f5649e3d05c
Anish Nambirajan <anish.nambi@gmail.com>
2020-12-12 21:14:12 -0800

    first commit

--- venv/Scripts/easy_install-3.6-script.py
+++ venv/Scripts/easy_install-3.6-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\anish\PycharmProjects\JumbledWords\venv\Scripts\python.exe
+#!C:\Users\anish\PycharmProjects\JumbledWords\JumbledWords\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install-3.6'
 __requires__ = 'setuptools==40.8.0'
 import re
