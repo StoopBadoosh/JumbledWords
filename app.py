8f98cf8905574470b979567d84b5f2e13da94a1e
Anish Nambirajan <anish.nambi@gmail.com>
2020-12-18 18:53:47 -0800

    adding only 1 word

--- app.py
+++ app.py
@@ -32,55 +32,17 @@ def jumble():
         return render_template('jumbled.html')
     else:
         input_for_jumbled_word1 = request.form['input_for_jumbled_word1'].strip()
-        input_for_jumbled_word2 = request.form['input_for_jumbled_word2'].strip()
-        input_for_jumbled_word3 = request.form['input_for_jumbled_word3'].strip()
-        input_for_jumbled_word4 = request.form['input_for_jumbled_word4'].strip()
-        input_for_jumbled_word5 = request.form['input_for_jumbled_word5'].strip()
+
         wordlist1 = list(input_for_jumbled_word1)
-        wordlist2 = list(input_for_jumbled_word2)
-        wordlist3 = list(input_for_jumbled_word3)
-        wordlist4 = list(input_for_jumbled_word4)
-        wordlist5 = list(input_for_jumbled_word5)
+
         random.shuffle(wordlist1)
-        random.shuffle(wordlist2)
-        random.shuffle(wordlist3)
-        random.shuffle(wordlist4)
-        random.shuffle(wordlist5)
+
         x = ''
         for loop in wordlist1:
             x = x + loop
         print(x)
         words1={'original_word':input_for_jumbled_word1,'jumbled word':x}
         mongo.db.jumbled_words.insert_one(words1)
-
-        x = ''
-        for loop in wordlist2:
-            x = x + loop
-        print(x)
-        words2 = {'original_word': input_for_jumbled_word2, 'jumbled word': x}
-        mongo.db.jumbled_words.insert_one(words2)
-
-        x = ''
-        for loop in wordlist3:
-            x = x + loop
-        print(x)
-        words3 = {'original_word': input_for_jumbled_word2, 'jumbled word': x}
-        mongo.db.jumbled_words.insert_one(words3)
-
-        x = ''
-        for loop in wordlist4:
-            x = x + loop
-        print(x)
-        words4 = {'original_word': input_for_jumbled_word4, 'jumbled word': x}
-        mongo.db.jumbled_words.insert_one(words4)
-
-        x = ''
-        for loop in wordlist5:
-            x = x + loop
-        print(x)
-        words5 = {'original_word': input_for_jumbled_word5, 'jumbled word': x}
-        mongo.db.jumbled_words.insert_one(words5)
-        flash('Your words have been successfully added', "success")
         return redirect('/jumble')
 
 @app.route('/solve', methods=['GET', 'POST'])
